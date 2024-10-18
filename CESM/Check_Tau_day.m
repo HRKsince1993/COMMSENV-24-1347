0162b5eabddd9cfb94f14e0e7346112577054ab3
HRKsicne1993 <141501295+HRKsince1993@users.noreply.github.com>
2024-10-19 03:14:48 +0800

    Add files via upload

---
+++ CESM/Check_Tau_day.m
@@ -0,0 +1,38 @@
+clc;clear
+
+path = 'F:\2023PMM_Work\Input_TauxA_WWB_2023Daily\';
+fext = '*.dat';
+struct = dir([path fext]);
+name = {struct.name}';
+
+path1 = 'I:\CESM\Output_Data\HRK_2023ENSO_Ensamble\Exp_NTAandTIOandPMMandSEPandWWB\Exp_NTAandTIOandPMMandSEPandWWB_0110_00\ocean_daily\';
+fext = '*.nc';
+struct = dir([path1 fext]);
+name1 = {struct.name}';
+clear fext struct;
+
+tlon = ncread([path1,name1{1}],'TLONG');
+tlat = ncread([path1,name1{1}],'TLAT');
+%%
+i1 = 314;
+bin = importtauday([path,'taux_day_',num2str(i1),'.dat']);
+
+contourf(tlon,tlat,bin')
+colorbar
+caxis([0,0.1])
+%%
+clear bin
+for i1 = 1:length(name)
+    bin(:,:,i1) = importtauday([path,'taux_day_',num2str(i1),'.dat']);
+end
+
+bin(bin == 0) = nan;
+%%
+fig1 = squeeze(nanmean(bin(:,166:210,:),2));
+fig_lon = mean(tlon(166:210,:),1);
+fig_lat = mean(tlat(166:210,:),1);
+%%
+% contourf(fig_lon,1:size(fig1,2),fig1')
+contourf(fig1')
+
+
---
+++ CESM/Make_Output_Folders.m
@@ -0,0 +1,30 @@
+clc;clear;
+
+for one = 41:41 % 1;Ctrl;2:NTA;3:PMM;4:TIO;5:TroPac:6£ºTPCtrl;7:NTA15;8:NTA30;9:AtlNino;10:NoWNP;11:NoPMM;12:NTAandTIO;13:TroPacNTA;14:PMMandNTA;15:TPCtrlG;16:SEP
+    % 17:NTAandTIOandSEP;18:NTAandTIOandPMMandSEP;19:NTAandTIOLgandPMMandSEP;20:CESM-TP;21:TO;22:NTAandTIOandPMMandSEPandWNP;23:NTAandTIOandEP;24:NTAandTIOandPMMandSEP
+    % 25:NTAandTIOandEPandWWB;26:NTAandTIOandPMMandSEPandWWB;27:NTAandTIOandSEPlg;28:NTAandTIOandSEPandWWB;29:NTAandTIOandSEPandWWBmay;30:TIOlg;31:NTAlg
+    % 32:NTAandTIOandSEPandWWBb;33:NTAandTIOandSEPandWWBbmay;34:PMMsmall;35:NTAandTIOandPMMandSEPlg;36:NETPCli;37:NTAandTIOandPMMandSEPandWWBb;
+    % 38:NTAandTIOandPMMandSEPandWWBbmay;39:NTAandTIOandSEPandWWBOctNov;40:TPCtrlnoWWB2;41:TPCtrlNoWWBolar
+    first_name = {'Ctrl','NTA','PMM','TIO','TroPac','TPCtrl','NTA15','NTA30','AtlNino','NoWNP','NoPMM','NTAandTIO','TroPacNTA','PMMandNTA','TPCtrlG','SEP'...
+        ,'NTAandTIOandSEP','NTAandTIOandPMMandSEP','NTAandTIOLgandPMMandSEP','CESM_TP','TO','NTAandTIOandPMMandSEPandWNP','NTAandTIOandEP','NTAandTIOandPMMandSEP_New'...
+        ,'NTAandTIOandEPandWWB','NTAandTIOandPMMandSEPandWWB','NTAandTIOandSEPlg','NTAandTIOandSEPandWWB','NTAandTIOandSEPandWWBmay','TIOlg','NTAlg'...
+        ,'NTAandTIOandSEPandWWBb','NTAandTIOandSEPandWWBbmay','PMMsmall','NTAandTIOandPMMandSEPlg','NETPCli','NTAandTIOandPMMandSEPandWWBb'...
+        ,'NTAandTIOandPMMandSEPandWWBbmay','NTAandTIOandSEPandWWBOctNov','TPCtrlNoWWB2','TPCtrlNoWWBolar'};
+    if one >= 3 && one <= 22
+        path = ['H:\CESM\Output_Data\HRK_2023ENSO_Ensamble\Exp_',first_name{one},'\Exp_',first_name{one},'_1217_']
+    elseif one >= 23 && one <= 39
+        path = ['H:\CESM\Output_Data\HRK_2023ENSO_Ensamble\Exp_',first_name{one},'\Exp_',first_name{one},'_0110_']
+    elseif one >= 40
+        path = ['H:\CESM\Output_Data\HRK_2023ENSO_Ensamble\Exp_',first_name{one},'\Exp_',first_name{one},'_0318_']
+    end
+    for i1 = 0:10
+        aimpath1 = [path,num2str(i1,'%2.2i'),'\atm\'];
+        aimpath2 = [path,num2str(i1,'%2.2i'),'\ocean_daily\'];
+        aimpath3 = [path,num2str(i1,'%2.2i'),'\ocean_monthly\'];
+        if exist(aimpath1,'dir')~=7
+            mkdir(aimpath1);
+            mkdir(aimpath2);
+            mkdir(aimpath3);
+        end
+    end
+end
